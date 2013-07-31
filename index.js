2899c66b8212d40d9c8de5a844ee93c841d5196d
Guo Yu <i@guoyu.me>
2013-07-31 22:47:09 +0800

    functions done, ship to 0.0.1

--- index.js
+++ index.js
@@ -5,13 +5,38 @@
 // /____/\__,_/ .___/\___/_/  /_/ /_/\___/_/   \____/ 
 //           /_/                                      
 // superhero is a api wrapper/spider of NPM
+
 var api = require('beer'),
-    cheerio = require('cheerio');
+    cheerio = require('cheerio'),
+    User = require('./lib/user'),
+    Pkg = require('./lib/pkg');
 
-exports.fetch = function(username,callback) {
+exports.fetch = function(username, callback) {
+    if (username && typeof(username) == 'string') {
+        api.get('https://npmjs.org/~' + username, null, function(err, result) {
+            if (!err) {
+                if (result.body) {
+                    var user = new User(username,result.body);
+                    if (user.username != '404') {
+                        user.meta();
+                        user.pkgs(function(user){
+                            callback(user);
+                        });
+                    } else {
+                        callback(user);                        
+                    }
+                }
+            } else {
+                console.log(err);
+            }
+        });
+    }
+}
+
+exports.vs = function(usernames, callback) {
 
 }
 
-exports.vs = function(usernames,callback) {
-
-}
+// exports.fetch('turing', function(user) {
+//     console.log(user);
+// });
